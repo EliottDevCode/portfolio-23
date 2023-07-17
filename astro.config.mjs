e8253556f578f9791a95694fee3b9443d74ae332
Eliott Gcv <eliott.gcv@gmail.com>
2023-07-17 15:20:56 +0200

    ssl local

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  plugins: [ basicSsl() ]
 });
